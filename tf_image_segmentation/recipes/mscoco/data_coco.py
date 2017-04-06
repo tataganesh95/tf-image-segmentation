aa55f32fb6a1f48ee354f751133e5e1138f95443
Andrew Hundt <ATHundt@gmail.com>
2017-04-05 22:19:23 -0400

    data_coco.py json to segmentation verbose param

--- tf_image_segmentation/recipes/mscoco/data_coco.py
+++ tf_image_segmentation/recipes/mscoco/data_coco.py
@@ -184,7 +184,7 @@ def coco_download(dataset_path, filenames, dataset_root, urls, md5s, annotation_
 
 
 @data_coco.command
-def coco_json_to_segmentation(seg_mask_output_paths, annotation_paths, seg_mask_image_paths):
+def coco_json_to_segmentation(seg_mask_output_paths, annotation_paths, seg_mask_image_paths, verbose):
     for (seg_mask_path, annFile, image_path) in zip(seg_mask_output_paths, annotation_paths, seg_mask_image_paths):
         print('Loading COCO Annotations File: ', annFile)
         print('Segmentation Mask Output Folder: ', seg_mask_path)
